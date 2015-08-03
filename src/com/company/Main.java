8d03653a1df0c8294d51b8224258326ded9149cb
lmast <pillravi@users.noreply.github.com>
2015-08-03 14:27:44 -0700

    'Convert Element to Collection' Version 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,16 @@
 package com.company;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     }
 
-    void start(Car car) {
-        car.start();
+    void start(List<Car> cars) {
+        for (Car car : cars) {
+            car.start();
+        }
     }
 }
