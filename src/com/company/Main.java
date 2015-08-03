58c5d92eaf9f9ec1b7acfc5c92c713cb95a9d834
lmast <pillravi@users.noreply.github.com>
2015-08-03 14:47:11 -0700

    'Wrap Code with Timer' V1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,4 +26,20 @@ public class Main {
             results.add(t.getResult());
         }
     }
+
+    void wrapCodeWithTimer() {
+        for (int i = 0; i < 1000; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private boolean isPrime(int num) {
+        if (num == 2 ) return true;
+        if (num % 2 == 0) return false;
+        for (int i = 3; i * i <= num; i += 2)
+            if (num % i == 0) return false;
+        return true;
+    }
 }
